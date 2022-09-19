9e7afee4dfd372f7e4e74e5caca4c66ec8436969
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-09-19 08:22:33 +0300

    Add Error.cs

---
+++ SDLTooSharp/Bindings/SDL2/Error.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SDLTooSharp.Bindings.SDL2;
+
+public static partial class SDL
+{
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDL_SetError([MarshalAs(UnmanagedType.LPUTF8Str)] string error);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetError")]
+    private static extern IntPtr _SDL_GetError();
+
+    public static string SDL_GetError() => PtrToManaged(_SDL_GetError());
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SDL_ClearError();
+}
