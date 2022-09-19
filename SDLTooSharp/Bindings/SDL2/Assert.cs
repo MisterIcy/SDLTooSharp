07c15b52e61e989e17fbdf4c4c0722dd76d27605
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-09-19 08:00:21 +0300

    Add Assert.cs

---
+++ SDLTooSharp/Bindings/SDL2/Assert.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace SDLTooSharp.Bindings.SDL2;
+
+public static partial class SDL
+{
+    /// <summary>
+    /// C# almost equivalent to Triggering a Breakpoint
+    /// </summary>
+    [DebuggerHidden]
+    [Conditional("DEBUG")]
+    public static void SDL_TriggerBreakpoint()
+    {
+        if (Debugger.IsAttached)
+        {
+            Debugger.Break();
+        }
+    }
+}
