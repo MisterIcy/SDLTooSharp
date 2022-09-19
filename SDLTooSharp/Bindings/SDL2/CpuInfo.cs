30d3f7b8faa02164e890bb94a1c920ec36b39ac7
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-09-19 08:19:50 +0300

    Add CpuInfo.cs

---
+++ SDLTooSharp/Bindings/SDL2/CpuInfo.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SDLTooSharp.Bindings.SDL2;
+
+public static partial class SDL
+{
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDL_GetCPUCount();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDL_GetCPUCacheLineSize();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasRDTSC();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasAltiVec();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasMMX();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_Has3DNow();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasSSE();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasSSE2();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasSSE3();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasSSE41();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasSSE42();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasAVX();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasAVX2();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasAVX512F();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasARMSIMD();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasNEON();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasLSX();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasLASX();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_GetSystemRAM();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern ulong SDL_SIMDGetAlignment();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDL_SIMDAlloc(ulong len);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDL_SIMDRealloc(IntPtr mem, ulong len);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SDL_SIMDFree(IntPtr mem);
+}
