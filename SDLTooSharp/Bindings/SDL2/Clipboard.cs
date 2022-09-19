1298916003864d24f31ff7af4b0501d1612a721a
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-09-19 08:15:32 +0300

    Add Clipboard.cs

---
+++ SDLTooSharp/Bindings/SDL2/Clipboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SDLTooSharp.Bindings.SDL2;
+
+public static partial class SDL
+{
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDL_SetClipboardText([MarshalAs(UnmanagedType.LPUTF8Str)] string text);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetClipboardText")]
+    private static extern IntPtr _SDL_GetClipboardText();
+
+    public static string SDL_GetClipboardText() => PtrToManaged(_SDL_GetClipboardText(), true);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasClipboardText();
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDL_SetPrimarySelectionText([MarshalAs(UnmanagedType.LPUTF8Str)] string text);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetPrimarySelectionText")]
+    private static extern IntPtr _SDL_GetPrimarySelectionText();
+
+    public static string SDL_GetPrimarySelectionText() => PtrToManaged(_SDL_GetPrimarySelectionText(), true);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool SDL_HasPrimarySelectionText();
+}
