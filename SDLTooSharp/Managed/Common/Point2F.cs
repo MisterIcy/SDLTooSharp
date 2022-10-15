696a5bc7f6b194aa41f501062b7a8692a4687b16
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-15 15:22:15 +0300

    Add Point2F.cs class & some tests

---
+++ SDLTooSharp/Managed/Common/Point2F.cs
@@ -0,0 +1,195 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using SDLTooSharp.Bindings.SDL2;
+
+namespace SDLTooSharp.Managed.Common;
+
+public class Point2F : IEquatable<Point2F>
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public Point2F(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point2F(float v) : this(v, v)
+    {
+    }
+
+    public Point2F() : this(0)
+    {
+    }
+
+#region Casting Operators
+
+    public static explicit operator SDL.SDL_FPoint(Point2F pt)
+    {
+        SDL.SDL_FPoint point = default;
+        point.X = pt.X;
+        point.Y = pt.Y;
+
+        return point;
+    }
+
+    public static implicit operator Point2F(SDL.SDL_FPoint pt)
+    {
+        return new Point2F(pt.X, pt.Y);
+    }
+
+    public static explicit operator PointF(Point2F pt)
+    {
+        PointF point = default;
+        point.X = pt.X;
+        point.Y = pt.Y;
+        return point;
+    }
+
+    public static implicit operator Point2F(PointF pt)
+    {
+        return new Point2F(pt.X, pt.Y);
+    }
+
+    public static explicit operator Point2(Point2F pt)
+    {
+        return new Point2((int)Math.Round(pt.X), (int)Math.Round(pt.Y));
+    }
+
+    public static explicit operator Point2F(Point2 pt)
+    {
+        return new Point2F(pt.X, pt.Y);
+    }
+
+#endregion
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Point2F left, Point2F right)
+    {
+        return Math.Abs(left.X - right.X) < float.Epsilon &&
+               Math.Abs(left.Y - right.Y) < float.Epsilon;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Point2F left, Point2F right)
+    {
+        return !(left == right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator -(Point2F pt)
+    {
+        unchecked
+        {
+            return new Point2F(-1 * Math.Abs(pt.X), -1 * Math.Abs(pt.Y));
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator +(Point2F pt)
+    {
+        return new Point2F(Math.Abs(pt.X), Math.Abs(pt.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator +(Point2F left, Point2F right)
+    {
+        unchecked
+        {
+            return new Point2F(left.X + right.X, left.Y + right.Y);
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator +(Point2F left, float right)
+    {
+        return left + new Point2F(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator +(float left, Point2F right)
+    {
+        return right + left;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator -(Point2F left, Point2F right)
+    {
+        return new Point2F(left.X - right.X, left.Y - right.Y);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator -(Point2F left, float right)
+    {
+        return left - new Point2F(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator -(float left, Point2F right)
+    {
+        return new Point2F(left) - right;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator *(Point2F left, Point2F right)
+    {
+        return new Point2F(left.X * right.X, left.Y * right.Y);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator *(Point2F left, float right)
+    {
+        return left * new Point2F(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator *(float left, Point2F right)
+    {
+        return right * left;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator /(Point2F left, Point2F right)
+    {
+        if (right.X == 0 || right.Y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+
+        return new Point2F(left.X / right.X, left.Y / right.Y);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator /(Point2F left, float right)
+    {
+        return left / new Point2F(right);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Point2F operator /(float left, Point2F right)
+    {
+        return new Point2F(left) / right;
+    }
+
+
+    public bool Equals(Point2F? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X.Equals(other.X) && Y.Equals(other.Y);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Point2F)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+}
---
+++ TooSharpTests/Managed/Common/Point2FTest.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class Point2FTest
+{
+    [Fact]
+    public void testEqualityOperator()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.4f);
+        Point2F p2 = new Point2F(1.3f, 2.4f);
+
+        Assert.True(p1 == p2);
+    }
+
+    [Fact]
+    public void testInequalityOperator()
+    {
+        Point2F p1 = new Point2F(1.34f, 3.14f);
+        Point2F p2 = new Point2F(1.34f, 3.14159f);
+
+        Assert.True(p1 != p2);
+    }
+
+    [Fact]
+    public void testUnaryMinusOperator()
+    {
+        Point2F p1 = new Point2F(3.14159f, -12.4f);
+        Point2F p2 = -p1;
+
+        Assert.Equal(-3.14159f, p2.X);
+        Assert.Equal(-12.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestUnaryPlusOperator()
+    {
+        Point2F p1 = new Point2F(3.14159f, -12.4f);
+        Point2F p2 = +p1;
+
+        Assert.Equal(3.14159f, p2.X);
+        Assert.Equal(12.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestAddTwoPoints()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        Point2F p2 = new Point2F(1.4f, 3.9f);
+
+        Point2F p3 = p1 + p2;
+        Assert.Equal(1.3f + 1.4f, p3.X);
+        Assert.Equal(2.2f + 3.9f, p3.Y);
+    }
+
+    [Fact]
+    public void TestAddPointAndScalar()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        float v = 3.4f;
+
+        Point2F p2 = p1 + v;
+        Assert.Equal(1.3f + 3.4f, p2.X);
+        Assert.Equal(2.2f + 3.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestAddScalarAndPoint()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        float v = 4.9f;
+
+        Point2F p2 = v + p1;
+        Assert.Equal(1.3f + 4.9f, p2.X);
+        Assert.Equal(2.2f + 4.9f, p2.Y);
+    }
+
+    [Fact]
+    public void TestSubtractPointFromPoint()
+    {
+        Point2F p1 = new Point2F(1.3f, 3.2f);
+        Point2F p2 = new Point2F(4.1f, 0.9f);
+
+        Point2F p3 = p1 - p2;
+        Point2F p4 = p2 - p1;
+
+        Assert.Equal(1.3f - 4.1f, p3.X);
+        Assert.Equal(3.2f - 0.9f, p3.Y);
+        Assert.Equal(4.1f - 1.3f, p4.X);
+        Assert.Equal(0.9f - 3.2f, p4.Y);
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestSubtractScalarFromPoint()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestSubtractPointFromScalar()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestMultiplyPoints()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestMultiplyPointWithScalar()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestMultiplyScalarWithPoint()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDividePoints()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDividePointsWithZero()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDividePointWithScalar()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDividePointWithZeroScalar()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDivideScalarWithZeroPoint()
+    {
+    }
+
+    [Fact(Skip = "Needs to be implemented")]
+    public void TestDivideScalarWithPoint()
+    {
+    }
+}
