10667927e3a6f345d42a15151bb1e827256c3bcb
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-15 17:07:31 +0300

    [Managed] Common Classes - Sizes (#6)
    * Add Size class and tests
    
    * Add missing fact in SizeTest.cs
    
    * Add SizeF clas
    
    * Aggressive Inlining

---
+++ TooSharpTests/Managed/Common/SizeFTest.cs
@@ -0,0 +1,330 @@
+﻿using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class SizeFTest
+{
+    [Fact]
+    public void TestCreateSize()
+    {
+        SizeF s1 = new SizeF(1.2f, 2.2f);
+        Assert.Equal(1.2f, s1.Width);
+        Assert.Equal(2.2f, s1.Height);
+        Assert.Equal(1.2f * 2.2f, s1.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithZeroWidth()
+    {
+        SizeF s1 = new SizeF(0.0f, 2.2f);
+        Assert.Equal(0.0f, s1.Width);
+        Assert.Equal(2.2f, s1.Height);
+        Assert.Equal(0, s1.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithZeroHeight()
+    {
+        SizeF s1 = new SizeF(1.2f, 0.0f);
+        Assert.Equal(1.2f, s1.Width);
+        Assert.Equal(0.0f, s1.Height);
+        Assert.Equal(0, s1.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithEpsilonWidth()
+    {
+        SizeF s1 = new SizeF(float.Epsilon, 2.2f);
+        Assert.Equal(float.Epsilon, s1.Width);
+        Assert.Equal(2.2f, s1.Height);
+        Assert.Equal(float.Epsilon * 2.2f, s1.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithEpsilonHeight()
+    {
+        SizeF s1 = new SizeF(1.2f, float.Epsilon);
+        Assert.Equal(1.2f, s1.Width);
+        Assert.Equal(float.Epsilon, s1.Height);
+        Assert.Equal(float.Epsilon * 1.2f, s1.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithNegativeWidth()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => { new SizeF(-1.2f, 1.2f); }
+        );
+    }
+
+    [Fact]
+    public void TestCreateSizeWIthNegativeHeight()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => { new SizeF(1.2f, -1.2f); });
+    }
+
+    [Fact]
+    public void TestEquality()
+    {
+        SizeF s1 = new SizeF(1.34f, 3.29f);
+        SizeF s2 = new SizeF(1.34f, 3.29f);
+
+        Assert.True(s1 == s2);
+    }
+
+    [Fact]
+    public void TestInequality()
+    {
+        SizeF s1 = new SizeF(1.34f, 3.29f);
+        SizeF s2 = new SizeF(1.34f, 3.23f);
+
+        Assert.True(s1 != s2);
+    }
+
+    [Fact]
+    public void TestAddSizeWithSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 1.9f);
+
+        SizeF s3 = s1 + s2;
+
+        Assert.Equal(s1.Width + s2.Width, s3.Width);
+        Assert.Equal(s1.Height + s2.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestAddSizeWithScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 1.9f;
+
+        SizeF s3 = s1 + v;
+
+        Assert.Equal(s1.Width + v, s3.Width);
+        Assert.Equal(s1.Height + v, s3.Height);
+    }
+
+    [Fact]
+    public void TestAddSizeWithNegativeScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = -1.9f;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = s1 + v;
+        });
+    }
+
+    [Fact]
+    public void TestAddScalarWithSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 1.9f;
+
+        SizeF s3 = v + s1;
+
+        Assert.Equal(s1.Width + v, s3.Width);
+        Assert.Equal(s1.Height + v, s3.Height);
+    }
+
+    [Fact]
+    public void TestAddNegativeScalarWithSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = -1.9f;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = v + s1;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeFromSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 1.9f);
+
+        SizeF s3 = s1 - s2;
+
+        Assert.Equal(s1.Width - s2.Width, s3.Width);
+        Assert.Equal(s1.Height - s2.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestSubtractScalarFromSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 1.9f;
+
+        SizeF s3 = s1 - v;
+
+        Assert.Equal(s1.Width - v, s3.Width);
+        Assert.Equal(s1.Height - v, s3.Height);
+    }
+
+    [Fact]
+    public void TestSubtractNegativeScalarFromSize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = -1.9f;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = s1 - v;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractScalarFromSizeResultingInNegativeDimension()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 2.3f;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = s1 - v;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeFromScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 8.9f;
+
+        SizeF s3 = v - s1;
+
+        Assert.Equal(v - s1.Width, s3.Width);
+        Assert.Equal(v - s1.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestSubtractSizeFromNegativeScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = -8.9f;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = v - s1;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeFromScalarResultingInNegativeDimension()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 1.9f;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            SizeF s3 = v - s1;
+        });
+    }
+
+    [Fact]
+    public void TestMultiplySizeBySize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 1.9f);
+
+        SizeF s3 = s1 * s2;
+
+        Assert.Equal(s1.Width * s2.Width, s3.Width);
+        Assert.Equal(s1.Height * s2.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestMultiplySizeByScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 2.3f;
+
+        SizeF s3 = s1 * v;
+
+        Assert.Equal(s1.Width * v, s3.Width);
+        Assert.Equal(s1.Height * v, s3.Height);
+    }
+
+    [Fact]
+    public void TestMultiplyScalarBySize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 2.3f;
+
+        SizeF s3 = v * s1;
+
+        Assert.Equal(s1.Width * v, s3.Width);
+        Assert.Equal(s1.Height * v, s3.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeBySize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 1.9f);
+
+        SizeF s3 = s1 / s2;
+
+        Assert.Equal(s1.Width / s2.Width, s3.Width);
+        Assert.Equal(s1.Height / s2.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeBySizeWithZeroDimension()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 0.0f);
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            SizeF s3 = s1 / s2;
+        });
+    }
+
+    [Fact]
+    public void TestDivideSizeByScalar()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 3.49f;
+
+        SizeF s3 = s1 / v;
+
+        Assert.Equal(s1.Width / v, s3.Width);
+        Assert.Equal(s1.Height / v, s3.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeByZero()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        SizeF s2 = new SizeF(1.3f, 0.0f);
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            SizeF s3 = s1 / 0;
+        });
+    }
+
+    [Fact]
+    public void TestDivideScalarBySize()
+    {
+        SizeF s1 = new SizeF(2.2f, 3.3f);
+        float v = 3.49f;
+
+        SizeF s3 = v / s1;
+
+        Assert.Equal(v / s1.Width, s3.Width);
+        Assert.Equal(v / s1.Height, s3.Height);
+    }
+
+    [Fact]
+    public void TestDivideScalarBySizeWithZeroDimensions()
+    {
+        SizeF s1 = new SizeF(2.2f, 0.0f);
+
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            SizeF s3 = 4.92f / s1;
+        });
+    }
+}
---
+++ TooSharpTests/Managed/Common/SizeTest.cs
@@ -0,0 +1,285 @@
+﻿using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class SizeTest
+{
+    [Fact]
+    public void TestCreateSize()
+    {
+        Size sz = new Size(2, 2);
+        Assert.Equal(2, sz.Width);
+        Assert.Equal(2, sz.Height);
+        Assert.Equal(2 * 2, sz.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithZeroWidth()
+    {
+        Size sz = new Size(0, 2);
+        Assert.Equal(0, sz.Width);
+        Assert.Equal(2, sz.Height);
+        Assert.Equal(0, sz.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithZeroHeight()
+    {
+        Size sw = new Size(2, 0);
+        Assert.Equal(2, sw.Width);
+        Assert.Equal(0, sw.Height);
+        Assert.Equal(0, sw.Area);
+    }
+
+    [Fact]
+    public void TestCreateSizeWithNegativeWidth()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var sz = new Size(-2, 2);
+        });
+    }
+
+    [Fact]
+    public void TestCreateSizeWithNegativeHeight()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var sz = new Size(2, -2);
+        });
+    }
+
+    [Fact]
+    public void TestSizeEquality()
+    {
+        Size s1 = new Size(2, 3);
+        Size s2 = new Size(2, 3);
+
+        Assert.True(s1 == s2);
+    }
+
+    [Fact]
+    public void TestInequality()
+    {
+        Size s1 = new Size(2, 3);
+        Size s2 = new Size(3, 2);
+
+        Assert.True(s1 != s2);
+    }
+
+    [Fact]
+    public void TestAddSizes()
+    {
+        Size s1 = new Size(2, 3);
+        Size s2 = new Size(3, 4);
+
+        Size s3 = s1 + s2;
+        Assert.Equal(5, s3.Width);
+        Assert.Equal(7, s3.Height);
+    }
+
+    [Fact]
+    public void TestAddSizeWithPositiveScalar()
+    {
+        Size s1 = new Size(2, 3);
+        int v = 3;
+
+        Size s2 = s1 + v;
+        Assert.Equal(5, s2.Width);
+        Assert.Equal(6, s2.Height);
+    }
+
+    [Fact]
+    public void TestAddSizeWithNegativeScalar()
+    {
+        Size s1 = new Size(2, 3);
+        int v = -1;
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            Size s2 = s1 + v;
+        });
+    }
+
+    [Fact]
+    public void TestAddSizeWithNegativeScalarDimensionBelowZero()
+    {
+        Size s1 = new Size(2, 3);
+        int v = -3;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            Size s2 = s1 + v;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeBySize()
+    {
+        Size s1 = new Size(3, 4);
+        Size s2 = new Size(2, 3);
+
+        Size s3 = s1 - s2;
+        Assert.Equal(1, s3.Width);
+        Assert.Equal(1, s3.Height);
+    }
+
+    [Fact]
+    public void TestSubtractSizeBySizeResultInNegativeDimension()
+    {
+        Size s1 = new Size(3, 4);
+        Size s2 = new Size(2, 3);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            Size s3 = s2 - s1;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeByScalar()
+    {
+        Size s1 = new Size(2, 3);
+        int v = 2;
+
+        Size s2 = s1 - v;
+        Assert.Equal(0, s2.Width);
+        Assert.Equal(1, s2.Height);
+    }
+
+    [Fact]
+    public void TestSubtractSizeByNegativeScalar()
+    {
+        Size s1 = new Size(2, 3);
+        int v = -12;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            Size s2 = s1 - v;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractSizeByScalarResultingInNegativeDimension()
+    {
+        Size s1 = new Size(4, 1);
+        int v = 2;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            Size s2 = s1 - v;
+        });
+    }
+
+    [Fact]
+    public void TestSubtractScalarBySize()
+    {
+        Size s1 = new Size(2, 3);
+        int v = 12;
+
+        Size s2 = v - s1;
+        Assert.Equal(10, s2.Width);
+        Assert.Equal(9, s2.Height);
+    }
+
+    [Fact]
+    public void TestMultiplySizeBySize()
+    {
+        Size s1 = new Size(2, 2);
+        Size s2 = new Size(4, 3);
+
+        Size s3 = s1 * s2;
+        Assert.Equal(8, s3.Width);
+        Assert.Equal(6, s3.Height);
+    }
+
+    [Fact]
+    public void TestMultiplySizeByScalar()
+    {
+        Size s1 = new Size(2, 4);
+        int v = 4;
+
+        Size s2 = s1 * v;
+        Assert.Equal(8, s2.Width);
+        Assert.Equal(16, s2.Height);
+    }
+
+    [Fact]
+    public void TestMultiplyScalarBySize()
+    {
+        Size s1 = new Size(2, 4);
+        int v = 4;
+
+        Size s2 = v * s1;
+        Assert.Equal(8, s2.Width);
+        Assert.Equal(16, s2.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeBySize()
+    {
+        Size s1 = new Size(20, 10);
+        Size s2 = new Size(4, 2);
+
+        Size s3 = s1 / s2;
+        Assert.Equal(5, s3.Width);
+        Assert.Equal(5, s3.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeBySizeWithZeroDimension()
+    {
+        Size s1 = new Size(20, 10);
+        Size s2 = new Size(4, 0);
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Size s3 = s1 / s2;
+        });
+    }
+
+    [Fact]
+    public void TestDivideSizeByScalar()
+    {
+        Size s1 = new Size(20, 10);
+        int v = 2;
+
+        Size s2 = s1 / v;
+        Assert.Equal(10, s2.Width);
+        Assert.Equal(5, s2.Height);
+    }
+
+    [Fact]
+    public void TestDivideSizeByScalarZero()
+    {
+        Size s1 = new Size(20, 10);
+        int v = 0;
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Size s2 = s1 / v;
+        });
+    }
+
+    [Fact]
+    public void TestDivideScalarBySize()
+    {
+        Size s1 = new Size(20, 10);
+        int v = 100;
+
+        Size s2 = v / s1;
+        Assert.Equal(5, s2.Width);
+        Assert.Equal(10, s2.Height);
+    }
+
+    [Fact]
+    public void TestDivideScalarByZeroSize()
+    {
+        Size s1 = new Size(20, 0);
+        int v = 20;
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Size s2 = v / s1;
+        });
+    }
+}
