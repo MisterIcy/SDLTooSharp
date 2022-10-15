1da00004e0a5b31f3febda8b53efc3eea26a3be3
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-15 15:36:44 +0300

    [Managed] Add Commonly Used Classes (Points) (#4)
    * Add a Point2 class to replace SDL-related structures
    
    * Add Point2F.cs class & some tests
    
    * Add tests
    
    * Update changelog [skip ci]

---
+++ TooSharpTests/Managed/Common/Point2FTest.cs
@@ -0,0 +1,218 @@
+﻿using System.Drawing;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class Point2FTest
+{
+    [Fact]
+    public void testEqualityOperator()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.4f);
+        Point2F p2 = new Point2F(1.3f, 2.4f);
+
+        Assert.True(p1 == p2);
+    }
+
+    [Fact]
+    public void testInequalityOperator()
+    {
+        Point2F p1 = new Point2F(1.34f, 3.14f);
+        Point2F p2 = new Point2F(1.34f, 3.14159f);
+
+        Assert.True(p1 != p2);
+    }
+
+    [Fact]
+    public void testUnaryMinusOperator()
+    {
+        Point2F p1 = new Point2F(3.14159f, -12.4f);
+        Point2F p2 = -p1;
+
+        Assert.Equal(-3.14159f, p2.X);
+        Assert.Equal(-12.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestUnaryPlusOperator()
+    {
+        Point2F p1 = new Point2F(3.14159f, -12.4f);
+        Point2F p2 = +p1;
+
+        Assert.Equal(3.14159f, p2.X);
+        Assert.Equal(12.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestAddTwoPoints()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        Point2F p2 = new Point2F(1.4f, 3.9f);
+
+        Point2F p3 = p1 + p2;
+        Assert.Equal(1.3f + 1.4f, p3.X);
+        Assert.Equal(2.2f + 3.9f, p3.Y);
+    }
+
+    [Fact]
+    public void TestAddPointAndScalar()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        float v = 3.4f;
+
+        Point2F p2 = p1 + v;
+        Assert.Equal(1.3f + 3.4f, p2.X);
+        Assert.Equal(2.2f + 3.4f, p2.Y);
+    }
+
+    [Fact]
+    public void TestAddScalarAndPoint()
+    {
+        Point2F p1 = new Point2F(1.3f, 2.2f);
+        float v = 4.9f;
+
+        Point2F p2 = v + p1;
+        Assert.Equal(1.3f + 4.9f, p2.X);
+        Assert.Equal(2.2f + 4.9f, p2.Y);
+    }
+
+    [Fact]
+    public void TestSubtractPointFromPoint()
+    {
+        Point2F p1 = new Point2F(1.3f, 3.2f);
+        Point2F p2 = new Point2F(4.1f, 0.9f);
+
+        Point2F p3 = p1 - p2;
+        Point2F p4 = p2 - p1;
+
+        Assert.Equal(1.3f - 4.1f, p3.X);
+        Assert.Equal(3.2f - 0.9f, p3.Y);
+        Assert.Equal(4.1f - 1.3f, p4.X);
+        Assert.Equal(0.9f - 3.2f, p4.Y);
+    }
+
+    [Fact]
+    public void TestSubtractScalarFromPoint()
+    {
+        Point2F p1 = new Point2F(2.3f, 4.29f);
+        float v = 3.14159f;
+
+        Point2F p2 = p1 - v;
+        Assert.Equal(2.3f - 3.14159f, p2.X);
+        Assert.Equal(4.29f - 3.14159f, p2.Y);
+    }
+
+    [Fact]
+    public void TestSubtractPointFromScalar()
+    {
+        Point2F p1 = new Point2F(2.3f, 4.29f);
+        float v = 3.14159f;
+
+        Point2F p2 = v - p1;
+        Assert.Equal(3.14159f - 2.3f, p2.X);
+        Assert.Equal(3.14159f - 4.29f, p2.Y);
+    }
+
+    [Fact]
+    public void TestMultiplyPoints()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        Point2F p2 = new Point2F(3.2f, 2.3f);
+
+        Point2F p3 = p1 * p2;
+        Assert.Equal(2.2f * 3.2f, p3.X);
+        Assert.Equal(3.3f * 2.3f, p3.Y);
+    }
+
+    [Fact]
+    public void TestMultiplyPointWithScalar()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        float v = 3.14159f;
+
+        Point2F p2 = p1 * v;
+        Assert.Equal(2.2f * v, p2.X);
+        Assert.Equal(3.3f * v, p2.Y);
+    }
+
+    [Fact]
+    public void TestMultiplyScalarWithPoint()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        float v = 3.14159f;
+
+        Point2F p2 = v * p1;
+        Assert.Equal(v * 2.2f, p2.X);
+        Assert.Equal(v * 3.3f, p2.Y);
+    }
+
+    [Fact]
+    public void TestDividePoints()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        Point2F p2 = new Point2F(3.2f, 2.3f);
+
+        Point2F p3 = p1 / p2;
+        Assert.Equal(2.2f / 3.2f, p3.X);
+        Assert.Equal(3.3f / 2.3f, p3.Y);
+    }
+
+    [Fact]
+    public void TestDividePointsWithZero()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        Point2F p2 = new Point2F(3.2f, 0.0f);
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Point2F p3 = p1 / p2;
+        });
+    }
+
+    [Fact]
+    public void TestDividePointWithScalar()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        float v = 3.14159f;
+
+        Point2F p2 = p1 / v;
+        Assert.Equal(2.2f / v, p2.X);
+        Assert.Equal(3.3f / v, p2.Y);
+    }
+
+    [Fact]
+    public void TestDividePointWithZeroScalar()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        float v = 0;
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Point2F pt2 = p1 / v;
+        });
+    }
+
+    [Fact]
+    public void TestDivideScalarWithZeroPoint()
+    {
+        Point2F p1 = new Point2F(2.2f, 0);
+        float v = 3.14159f;
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Point2F pt2 = v / p1;
+        });
+    }
+
+    [Fact]
+    public void TestDivideScalarWithPoint()
+    {
+        Point2F p1 = new Point2F(2.2f, 3.3f);
+        float v = 3.14159f;
+
+        Point2F p2 = v / p1;
+        Assert.Equal(v / 2.2f, p2.X);
+        Assert.Equal(v / 3.3f, p2.Y);
+    }
+}
---
+++ TooSharpTests/Managed/Common/Point2Test.cs
@@ -0,0 +1,228 @@
+﻿using System.Drawing;
+using SDLTooSharp.Bindings.SDL2;
+using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class Point2Test
+{
+    [Fact]
+    public void TestUnaryPlus()
+    {
+        var point = new Point2(-1, -2);
+        var test = +point;
+
+        Assert.Equal(1, test.X);
+        Assert.Equal(2, test.Y);
+    }
+
+    [Fact]
+    public void TestUnaryMinus()
+    {
+        var pt = new Point2(42, -12);
+        var test = -pt;
+
+        Assert.Equal(-42, test.X);
+        Assert.Equal(-12, test.Y);
+    }
+
+    [Fact]
+    public void TestEqualPoints()
+    {
+        var pt1 = new Point2(10, 10);
+        var pt2 = new Point2(5, 5);
+        pt2 += 5;
+
+        Assert.True(pt1 == pt2);
+    }
+
+    [Fact]
+    public void TestUnequalPoints()
+    {
+        var pt1 = new Point2(2);
+        var pt2 = new Point2(3);
+
+        Assert.True(pt1 != pt2);
+    }
+
+    [Fact]
+    public void TestAddPoints()
+    {
+        var pt1 = new Point2(3, 4);
+        var pt2 = new Point2(4, 5);
+
+        var testPoint = new Point2(3 + 4, 4 + 5);
+        Assert.Equal(testPoint, (pt1 + pt2));
+    }
+
+    [Fact]
+    public void TestAddPointWithInt()
+    {
+        var pt1 = new Point2(3, 4);
+        var testPoint = new Point2(3 + 5, 4 + 5);
+
+        Assert.Equal(testPoint, (pt1 + 5));
+    }
+
+    [Fact]
+    public void TestAddIntWithPoint()
+    {
+        var pt1 = new Point2(3, 4);
+        var testPoint = new Point2(3 + 8, 4 + 8);
+
+        Assert.Equal(testPoint, (8 + pt1));
+    }
+
+    [Fact]
+    public void TestSubtractPoints()
+    {
+        var pt1 = new Point2(3);
+        var pt2 = new Point2(2);
+
+        var testPoint = new Point2(3 - 2);
+        Assert.Equal(testPoint, (pt1 - pt2));
+    }
+
+    [Fact]
+    public void TestSubtractIntFromPoint()
+    {
+        var pt1 = new Point2(4, 5);
+
+        var testPoint = new Point2(4 - 4, 5 - 4);
+        Assert.Equal(testPoint, (pt1 - 4));
+    }
+
+    [Fact]
+    public void TestSubtractPointFromInt()
+    {
+        var pt1 = new Point2(8, 5);
+
+        var testPoint = new Point2(3 - 8, 3 - 5);
+        Assert.Equal(testPoint, (3 - pt1));
+    }
+
+    [Fact]
+    public void TestCastSDLPointToPoint2()
+    {
+        SDL.SDL_Point pt = default;
+        pt.X = 2;
+        pt.Y = 3;
+
+        var point = (Point2)pt;
+        Assert.Equal(2, point.X);
+        Assert.Equal(3, point.Y);
+    }
+
+    [Fact]
+    public void TestCastPoint2ToSDLPoint()
+    {
+        var pt = new Point2(3, 6);
+        var sdlPoint = (SDL.SDL_Point)pt;
+
+        Assert.Equal(3, sdlPoint.X);
+        Assert.Equal(6, sdlPoint.Y);
+    }
+
+    [Fact]
+    public void TestCastSystemPointToPoint2()
+    {
+        Point pt = default;
+        pt.X = 2;
+        pt.Y = 3;
+
+        var point = (Point2)pt;
+        Assert.Equal(2, point.X);
+        Assert.Equal(3, point.Y);
+    }
+
+    [Fact]
+    public void TestCastPoint2ToSystemPoint()
+    {
+        var pt = new Point2(3, 6);
+        var sdlPoint = (Point)pt;
+
+        Assert.Equal(3, sdlPoint.X);
+        Assert.Equal(6, sdlPoint.Y);
+    }
+
+    [Fact]
+    public void TestMultiplyPoints()
+    {
+        var pt1 = new Point2(3, 4);
+        var pt2 = new Point2(4, 5);
+
+        var testPoint = new Point2(3 * 4, 4 * 5);
+        Assert.Equal(testPoint, (pt1 * pt2));
+    }
+
+    [Fact]
+    public void TestMultiplyPointWithInt()
+    {
+        var pt1 = new Point2(3, 4);
+        var testPoint = new Point2(3 * 9, 4 * 9);
+
+        Assert.Equal(testPoint, (pt1 * 9));
+    }
+
+    [Fact]
+    public void TestMultiplyIntWithPoint()
+    {
+        var pt1 = new Point2(3, 4);
+        var testPoint = new Point2(3 * 3, 3 * 4);
+
+        Assert.Equal(testPoint, (3 * pt1));
+    }
+
+    [Fact]
+    public void TestDivideByPoint()
+    {
+        var pt1 = new Point2(20, 10);
+        var pt2 = new Point2(4, 2);
+
+        var testPoint = new Point2(5, 5);
+
+        Assert.Equal(testPoint, (pt1 / pt2));
+    }
+
+    [Fact]
+    public void TestDividePointByInt()
+    {
+        var pt1 = new Point2(20, 10);
+
+        var testPoint = new Point2(10, 5);
+
+        Assert.Equal(testPoint, (pt1 / 2));
+    }
+
+    [Fact]
+    public void TestDividePointByZeroInt()
+    {
+        var pt1 = new Point2(10, 20);
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            var result = pt1 / 0;
+        });
+    }
+
+    [Fact]
+    public void TestDivideByZeroPoint()
+    {
+        var pt1 = new Point2(3);
+        var pt2 = new Point2();
+
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            var result = pt1 / pt2;
+        });
+    }
+
+    [Fact]
+    public void TestDivideIntByPoint()
+    {
+        var pt1 = new Point2(5, 10);
+
+        var testPoint = new Point2(20, 10);
+        Assert.Equal(testPoint, (100 /pt1));
+
+    }
+}
