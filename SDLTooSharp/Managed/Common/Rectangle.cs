0ccd4bdc9adfe0260bc17f9f07b12da3a628a373
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-17 08:07:43 +0300

    [Managed] Common Classes - Rectangles (#7)
    * Work on Rectangle
    
    * Add intersection
    
    * Finish work on Rectangles

---
+++ SDLTooSharp/Managed/Common/Rectangle.cs
@@ -0,0 +1,203 @@
+﻿using System.Drawing;
+using SDLTooSharp.Bindings.SDL2;
+
+namespace SDLTooSharp.Managed.Common;
+
+/// <summary>
+/// Represents a Rectangle
+/// </summary>
+public class Rectangle : IEquatable<Rectangle>
+{
+    private Point2 _origin;
+
+    public Point2 Origin
+    {
+        get => _origin;
+        set => _origin = value;
+    }
+
+    private Size _dimensions;
+
+    public Size Dimensions
+    {
+        get => _dimensions;
+        set => _dimensions = value;
+    }
+
+    public int Area => Dimensions.Area;
+
+    public int X => Origin.X;
+    public int Y => Origin.Y;
+    public int Width => Dimensions.Width;
+    public int Height => Dimensions.Height;
+
+    public bool IsEmpty => Width == 0 || Height == 0;
+
+    public int Left => Origin.X;
+    public int Top => Origin.Y;
+    public int Right => Origin.X + Dimensions.Width;
+    public int Bottom => Origin.Y + Dimensions.Height;
+
+    public Rectangle(Point2 origin, Size dimensions)
+    {
+        Origin = origin;
+        Dimensions = dimensions;
+    }
+
+    public Rectangle(int x, int y, int width, int height) : this(new Point2(x, y), new Size(width, height))
+    {
+    }
+
+    public Rectangle() : this(0, 0, 0, 0)
+    {
+    }
+
+    public static Rectangle FromLTRB(int left, int top, int right, int bottom)
+    {
+        return new Rectangle(left, top, right - left, bottom - top);
+    }
+
+#region Casting Operators
+
+    public static explicit operator SDL.SDL_Rect(Rectangle r)
+    {
+        SDL.SDL_Rect rect = new SDL.SDL_Rect()
+        {
+            X = r.X,
+            Y = r.Y,
+            W = r.Width,
+            H = r.Height
+        };
+        return rect;
+    }
+
+    public static implicit operator Rectangle(SDL.SDL_Rect r)
+    {
+        return new Rectangle(r.X, r.Y, r.W, r.H);
+    }
+
+    public static explicit operator System.Drawing.Rectangle(Rectangle r)
+    {
+        System.Drawing.Rectangle rect = new System.Drawing.Rectangle()
+        {
+            X = r.X,
+            Y = r.Y,
+            Width = r.Width,
+            Height = r.Height
+        };
+
+        return rect;
+    }
+
+    public static implicit operator Rectangle(System.Drawing.Rectangle r)
+    {
+        return new Rectangle(r.X, r.Y, r.Width, r.Height);
+    }
+
+#endregion
+
+    public static bool operator ==(Rectangle left, Rectangle right)
+    {
+        return left.X == right.X &&
+               left.Y == right.Y &&
+               left.Width == right.Width &&
+               left.Height == right.Height;
+    }
+
+    public static bool operator !=(Rectangle left, Rectangle right)
+    {
+        return !(left == right);
+    }
+
+    public bool ContainsPoint(Point2 pt)
+    {
+        return pt.X >= Left && pt.X <= Right &&
+               pt.Y >= Top && pt.Y <= Bottom;
+    }
+
+    public bool ContainsRectangle(Rectangle other)
+    {
+        bool v1 = ContainsPoint(other.Origin);
+        bool v2 =
+            ContainsPoint(other.Origin + new Point2(other.Width, other.Height));
+
+        return v1 && v2;
+    }
+
+    public static bool Intersects(Rectangle r1, Rectangle r2)
+    {
+        return r1.IntersectsWith(r2);
+    }
+
+    public bool IntersectsWith(Rectangle other)
+    {
+        if (IsEmpty || other.IsEmpty)
+        {
+            return false;
+        }
+
+
+        return Math.Max(Left, other.Left) < Math.Min(Right, other.Right) &&
+               Math.Max(Top, other.Top) < Math.Min(Bottom, other.Bottom);
+    }
+
+    public Rectangle Intersection(Rectangle other)
+    {
+        if (!IntersectsWith(other))
+        {
+            return new Rectangle();
+        }
+
+        int left = Math.Max(Left, other.Left);
+        int right = Math.Min(Right, other.Right);
+        int top = Math.Max(Top, other.Top);
+        int bottom = Math.Min(Bottom, other.Bottom);
+
+        return new Rectangle(left, top, right - left, bottom - top);
+    }
+
+    public Rectangle Union(Rectangle other)
+    {
+        var left = Math.Min(Left, other.Left);
+        var top = Math.Min(Top, other.Top);
+        var right = Math.Max(Right, other.Right);
+        var bottom = Math.Max(Bottom, other.Bottom);
+
+        return Rectangle.FromLTRB(left, top, right, bottom);
+    }
+
+    public void Offset(Point2 offset)
+    {
+        Origin += offset;
+    }
+
+    public Rectangle Inflate(Size size)
+    {
+        return new Rectangle(
+            X - (size.Width / 2),
+            Y - (size.Height / 2),
+            Width + size.Width,
+            Height + size.Height
+        );
+    }
+
+    public bool Equals(Rectangle? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _origin.Equals(other._origin) && _dimensions.Equals(other._dimensions);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Rectangle)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_origin, _dimensions);
+    }
+}
---
+++ TooSharpTests/Managed/Common/RectangleTest.cs
@@ -0,0 +1,288 @@
+﻿using System.Security.Cryptography;
+using SDLTooSharp.Bindings.SDL2;
+using SDLTooSharp.Managed.Common;
+
+namespace TooSharpTests.Managed.Common;
+
+public class RectangleTest
+{
+    [Fact]
+    public void CreateRectangleWithOriginAndDimensions()
+    {
+        Point2 origin = new Point2(1, 2);
+        Size dimensions = new Size(2, 2);
+        Rectangle r = new Rectangle(origin, dimensions);
+
+        Assert.Equal(origin.X, r.X);
+        Assert.Equal(origin.X, r.Left);
+        Assert.Equal(origin.Y, r.Y);
+        Assert.Equal(origin.Y, r.Top);
+        Assert.Equal(dimensions.Width, r.Width);
+        Assert.Equal(dimensions.Height, r.Height);
+        Assert.Equal(dimensions.Area, r.Area);
+        Assert.Equal(origin.X + dimensions.Width, r.Right);
+        Assert.Equal(origin.Y + dimensions.Height, r.Bottom);
+    }
+
+    [Fact]
+    public void CreateRectangleWithCoordinatesAndIntegerDimensions()
+    {
+        Rectangle r = new Rectangle(1, 2, 2, 2);
+
+        Assert.Equal(1, r.X);
+        Assert.Equal(2, r.Y);
+        Assert.Equal(2, r.Width);
+        Assert.Equal(2, r.Height);
+        Assert.Equal(4, r.Area);
+        Assert.Equal(3, r.Right);
+        Assert.Equal(4, r.Bottom);
+    }
+
+    [Fact]
+    public void CreateEmptyRectangle()
+    {
+        Rectangle r = new Rectangle();
+
+        Assert.Equal(0, r.X);
+        Assert.Equal(0, r.Y);
+        Assert.Equal(0, r.Width);
+        Assert.Equal(0, r.Height);
+        Assert.Equal(0, r.Area);
+        Assert.True(r.IsEmpty);
+    }
+
+    [Fact]
+    public void TestCastSDLRectToRectangle()
+    {
+        SDL.SDL_Rect rect = default;
+        rect.X = 2;
+        rect.Y = 3;
+        rect.W = 10;
+        rect.H = 20;
+
+        Rectangle cvt = rect;
+        Assert.Equal(rect.X, cvt.X);
+        Assert.Equal(rect.Y, cvt.Y);
+        Assert.Equal(rect.W, cvt.Width);
+        Assert.Equal(rect.H, cvt.Height);
+    }
+
+    [Fact]
+    public void TestCastRectangleToSDLRect()
+    {
+        Rectangle rect = new Rectangle(2, 3, 10, 20);
+        SDL.SDL_Rect cvt = (SDL.SDL_Rect)rect;
+
+        Assert.Equal(rect.X, cvt.X);
+        Assert.Equal(rect.Y, cvt.Y);
+        Assert.Equal(rect.Width, cvt.W);
+        Assert.Equal(rect.Height, cvt.H);
+    }
+
+    [Fact]
+    public void TestCastSystemRectToRectangle()
+    {
+        System.Drawing.Rectangle rect = default;
+        rect.X = 2;
+        rect.Y = 3;
+        rect.Width = 10;
+        rect.Height = 20;
+
+        Rectangle cvt = rect;
+        Assert.Equal(rect.X, cvt.X);
+        Assert.Equal(rect.Y, cvt.Y);
+        Assert.Equal(rect.Width, cvt.Width);
+        Assert.Equal(rect.Height, cvt.Height);
+    }
+
+    [Fact]
+    public void TestCastRectangleToSystemRectangle()
+    {
+        Rectangle rect = new Rectangle(2, 3, 10, 20);
+        System.Drawing.Rectangle cvt = (System.Drawing.Rectangle)rect;
+
+        Assert.Equal(rect.X, cvt.X);
+        Assert.Equal(rect.Y, cvt.Y);
+        Assert.Equal(rect.Width, cvt.Width);
+        Assert.Equal(rect.Height, cvt.Height);
+    }
+
+    [Fact]
+    public void TestEqualityOperator()
+    {
+        Rectangle r1 = new Rectangle(1, 2, 3, 4);
+        Rectangle r2 = new Rectangle(
+            new Point2(1, 2),
+            new Size(3, 4));
+
+        Assert.True(r1 == r2);
+    }
+
+    [Fact]
+    public void TestInequalityOperator()
+    {
+        Rectangle r1 = new Rectangle(1, 2, 3, 4);
+        Rectangle r2 = new Rectangle(
+            new Point2(4, 5),
+            new Size(5, 6)
+        );
+
+        Assert.True(r1 != r2);
+    }
+
+    [Fact]
+    public void TestContainsPoint()
+    {
+        Rectangle rect = new Rectangle(1, 1, 4, 4);
+        Point2 pt = new Point2(3, 3);
+
+        Assert.True(rect.ContainsPoint(pt));
+    }
+
+    [Fact]
+    public void TestDoesNotContainPoint()
+    {
+        Rectangle rect = new Rectangle(1, 1, 4, 4);
+        Point2 pt = new Point2(0, 0);
+
+        Assert.False(rect.ContainsPoint(pt));
+    }
+
+    [Fact]
+    public void TestRectangleContainsRectangle()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 20, 20);
+        Rectangle r2 = new Rectangle(12, 12, 4, 4);
+
+        Assert.True(r1.ContainsRectangle(r2));
+    }
+
+    [Fact]
+    public void TestRectangleDoesNotContainRectangleDueToIntersect()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 20, 20);
+        Rectangle r2 = new Rectangle(12, 12, 40, 40);
+
+        Assert.False(r1.ContainsRectangle(r2));
+    }
+
+    [Fact]
+    public void TestIntersectingRectangles()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(5, 5, 10, 50);
+
+        Assert.True(r1.IntersectsWith(r2));
+        Assert.True(Rectangle.Intersects(r1, r2));
+    }
+
+    [Fact]
+    public void TestNonIntersectingRectangles()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(40, 40, 5, 5);
+
+        Assert.False(r1.IntersectsWith(r2));
+        Assert.False(Rectangle.Intersects(r1, r2));
+    }
+
+    [Fact]
+    public void TestIntersectWithEmptyRectangle()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(0, 0, 0, 0);
+
+        Assert.False(r1.IntersectsWith(r2));
+        Assert.False(Rectangle.Intersects(r1, r2));
+    }
+
+    [Fact]
+    public void TestNotEqualRectangles()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(0, 0, 0, 0);
+        
+        Assert.False(r1.Equals(r2));
+    }
+
+    [Fact]
+    public void TestEqualRectangles()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(10, 10, 10, 10);
+        
+        Assert.True(r1.Equals(r2));
+    }
+
+    [Fact]
+    public void TestIntersectionRectangleForIntersectingRects()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(18, 18, 20, 20);
+
+        Rectangle intersection = new Rectangle(18, 18, 2, 2);
+        
+        Assert.Equal(intersection, r1.Intersection(r2));
+    }
+
+    [Fact]
+    public void TestIntersectionRectangleForNonIntersectingRects()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Rectangle r2 = new Rectangle(0, 0, 2, 3);
+
+        Rectangle intersection = new Rectangle(0, 0, 0, 0);
+        
+        Assert.Equal(intersection, r1.Intersection(r2));
+    }
+
+    [Fact]
+    public void TestOffset()
+    {
+        Rectangle r1 = new Rectangle(5, 2, 2, 2);
+        Point2 pt = new Point2(3, 7);
+        
+        r1.Offset(pt);
+        
+        Assert.Equal(8, r1.Origin.X);
+        Assert.Equal(9, r1.Origin.Y);
+    }
+
+    [Fact]
+    public void TestInflate()
+    {
+        Rectangle r1 = new Rectangle(10, 10, 10, 10);
+        Size sz = new Size(4, 4);
+
+        Rectangle inflated = r1.Inflate(sz);
+        
+        Assert.Equal(8, inflated.X);
+        Assert.Equal(8, inflated.Y);
+        Assert.Equal(14, inflated.Width);
+        Assert.Equal(14, inflated.Height);
+    }
+
+    [Fact]
+    public void TestFromLTRB()
+    {
+        Rectangle r1 = Rectangle.FromLTRB(10, 10, 20, 20);
+        Assert.Equal(10, r1.Left);
+        Assert.Equal(10, r1.Top);
+        Assert.Equal(10, r1.Width);
+        Assert.Equal(10, r1.Height);
+    }
+
+    [Fact]
+    public void TestUnionRectangle()
+    {
+        Rectangle r1 = new Rectangle(2, 3, 10, 12);
+        Rectangle r2 = new Rectangle(5, 1, 21, 8);
+
+        Rectangle union = r1.Union(r2);
+        
+        Assert.Equal(2, union.Left);
+        Assert.Equal(1, union.Top);
+        Assert.Equal(26, union.Right);
+        Assert.Equal(15, union.Bottom);
+    }
+}
