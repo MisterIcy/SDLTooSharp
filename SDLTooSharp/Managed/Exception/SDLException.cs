e577a71005e044d986c628e1c93da018cca5571b
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-12-23 21:05:07 +0200

    Update SDLException.cs
    Add new property to contain the actual error reported from SDL
    
    Signed-off-by: Alexandros Koutroulis <icyd3mon@gmail.com>

--- SDLTooSharp/Managed/Exception/SDLException.cs
+++ SDLTooSharp/Managed/Exception/SDLException.cs
@@ -1,9 +1,23 @@
+using SDLTooSharp.Bindings.SDL2;
+
 namespace SDLTooSharp.Managed.Exception;
 
 public abstract class SDLException : System.Exception
 {
+    /// <summary>
+    /// Gets the error message reported by SDL.
+    /// </summary>
+    public string SdlErrorMsg { get; }
+    
     protected SDLException(string message) : base(message)
     {
-
+        try
+        {
+            SdlErrorMsg = SDL.SDL_GetError();
+        } catch ( DllNotFoundException )
+        {
+            // In case the application cannot find the SDL Library:
+            SdlErrorMsg = "Unable to find the SDL library";
+        }
     }
 }
