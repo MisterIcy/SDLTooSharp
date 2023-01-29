b0303e0ca4697e220222e1687213ad407c544567
Alexandros Koutroulis <icyd3mon@gmail.com>
2023-01-29 10:57:39 +0200

    Add Window Display Changed Event

---
+++ SDLTooSharp/Managed/Event/Window/WindowDisplayChangedEventArgs.cs
@@ -0,0 +1,24 @@
+using SDLTooSharp.Bindings.SDL2;
+using SDLTooSharp.Managed.Event.Attributes;
+using SDLTooSharp.Managed.Exception.Event;
+
+namespace SDLTooSharp.Managed.Event;
+
+/// <summary>
+/// Event args for when a Window is moved to another display
+/// </summary>
+[AcceptableWindowEvent(SDL.SDL_WindowEventID.SDL_WINDOWEVENT_DISPLAY_CHANGED)]
+public sealed class WindowDisplayChangedEventArgs : AbstractWindowEventArgs
+{
+
+    /// <summary>
+    /// Creates a new WindowDisplayChangedEventArgs object
+    /// </summary>
+    /// <remarks>The display where the window moved is stored in <see cref="AbstractWindowEventArgs.Data1"/>.</remarks>
+    /// <param name="evt">The SDL_Event structure containing the event's information</param>
+    /// <exception cref="InvalidEventTypeException">In case the event type is unacceptable. </exception>
+    /// <exception cref="EventClassMissingAttributeException">In case the child class is not annotated with a <see cref="AcceptableEventTypeAttribute"/> and as such the check cannot be performed</exception>
+    public WindowDisplayChangedEventArgs(SDL.SDL_Event evt) : base(evt)
+    {
+    }
+}
